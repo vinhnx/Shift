0cbee4e84b0a5059f045ed26d388abe3aacdc85e
Vinh Nguyen <vinhnguyen2308@gmail.com>
2021-11-28 10:43:22 +0700

    Add basic test

--- Tests/ShiftTests/EventKitWrapperTests.swift
+++ Tests/ShiftTests/EventKitWrapperTests.swift
@@ -1,10 +1,11 @@
 import XCTest
+import Shift
+import EventKit
 
 final class ShiftTests: XCTestCase {
-    func testExample() {
+    func testConfigureAppName() {
+        let name = "MyApp"
+        Shift.configureWithAppName(name)
+        XCTAssertEqual(Shift.appName, name)
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
